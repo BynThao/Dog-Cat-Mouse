01051060b624949141fa08ba658724739a1d4851
Longg011120 <duonglongkm@gmail.com>
2019-07-26 09:19:31 +0700

    sayhi method for dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,12 @@
 function Dog() {
 	this.stomach = [];	
 }
-dog.prototype.eat = function(cat) {
+Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+
+Dog.prototype.sayhi = function() {
+	console.log('hi I am Dog. My name is ' + chalk.blue(this.name));
+};
+
+module.exports = Dog;
